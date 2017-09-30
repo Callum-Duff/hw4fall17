0e6cea07b5477e138850e091d1099e9879ed06cc
Callum Duff <callum-duff@uiowa.edu>
2017-09-30 01:12:04 +0000

    adding check to make sure user id is unique

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
-    def user_params
-        params.require(:user).permit(:id, :email)
-    end
+  def user_params
+    params.require(:user).permit(:id, :email)
+  end
     
-    def new
-        # default: render 'new' template
-    end
+  def new
+    # default: render 'new' template
+  end
+    
+  def create
+    if User.exists?(id: params[:id])
+        flash[:notice] = "Sorry, that user-id is taken. Try again."
+    else
+        @user = User.create(user_params)
+        flash[:notice] = "Welcome #{@user.id}. Your account has been created!"
+        redirect_to movies_path
+    
+  end
 end
