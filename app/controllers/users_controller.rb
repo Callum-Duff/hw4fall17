7909b6b21438ba4d72cc5dc701644a5e082ddcc1
Callum Duff <callum-duff@uiowa.edu>
2017-09-30 00:29:43 +0000

    Working on new user form and redirection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def user_params
-        params.require(:user).permit(:id, :password)
+        params.require(:user).permit(:id, :email)
     end
     
     def new
