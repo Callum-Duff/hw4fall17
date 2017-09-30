6017b22078da9c64e2bddf93ecd8f380b3071c17
Callum Duff <callum-duff@uiowa.edu>
2017-09-30 21:20:39 +0000

    changed database schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,5 +21,10 @@ ActiveRecord::Schema.define(version: 20150809022253) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+  
+  create_table "users", force: :cascade do |t|
+    t.string   "id"
+    t.string   "email"
+  end
 
 end
