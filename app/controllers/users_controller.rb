be3927188d93fdde326711617e217810b0ff81ea
Callum Duff <callum-duff@uiowa.edu>
2017-10-01 04:09:12 +0000

    adding unique id checks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,13 @@ class UsersController < ApplicationController
   end
     
   def create
-    #if User.exists?(id: params[:id])
-    #    flash[:notice] = "Sorry, that user-id is taken. Try again."
-    #else
-    @user = User.create!(user_params)
+    if User.exists?(userid: params[:userid])
+      flash[:notice] = "Sorry, that user-id is taken. Try again."
+    else
+      @user = User.create!(user_params)
     #flash[:notice] = "#{user_params}"
-    flash[:notice] = "Welcome #{@user.userid}. Your account has been created!"
+      flash[:notice] = "Welcome #{@user.userid}. Your account has been created!"
+    end
     redirect_to movies_path
-    #end
-    
   end
 end
