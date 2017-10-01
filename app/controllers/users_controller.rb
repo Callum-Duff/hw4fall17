f0eb1dae5eae31a03d4e41596a93ba4123df9e71
Callum Duff <callum-duff@uiowa.edu>
2017-10-01 14:39:30 +0000

    reverting to previous routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,9 @@ class UsersController < ApplicationController
     if User.find_by_user_id(user_params[:user_id]).blank?
       @user = User.create_user!(user_params)
       flash[:notice] = "Welcome #{@user.user_id}. Your account has been created!"
-      redirect_to login_path
     else
       flash[:notice] = "Sorry, that user-id is taken. Try again."
-      redirect_to movies_path
     end
+    redirect_to login_path
   end
 end
