2e9c9e61c0476243f09f1943db05c0f433008fb7
Callum Duff <callum-duff@uiowa.edu>
2017-10-01 14:02:58 +0000

    testing things

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   def self.create_user!(userHash)
       userHash[:session_token] = SecureRandom.base64
-      self.create!(userHash)
+      create!(userHash)
   end
 end
