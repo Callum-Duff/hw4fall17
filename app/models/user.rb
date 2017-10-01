ee8346f9f4d86e8369e683192471953301077953
Callum Duff <callum-duff@uiowa.edu>
2017-10-01 13:45:29 +0000

    added self.create_user! to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  def create_user!(userHash)
+  def self.create_user!(userHash)
       userHash[:session_token] = SecureRandom.base64
       self.create!(userHash)
   end
