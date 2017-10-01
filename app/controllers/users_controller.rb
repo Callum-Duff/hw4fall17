f58239cc37f9e2c71cf5f9d69eaf13d6ee1598b2
Callum Duff <callum-duff@uiowa.edu>
2017-10-01 13:40:58 +0000

    changed users_controller to use create_user! method instead of create!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
     
   def create
     if User.find_by_userid(user_params[:userid]).blank?
-      @user = User.create!(user_params)
-      #flash[:notice] = "#{user_params}"
+      @user = User.create_user!(user_params)
       flash[:notice] = "Welcome #{@user.userid}. Your account has been created!"
     else
       flash[:notice] = "Sorry, that user-id is taken. Try again."
